8c755f7d0646a321ce180c71c940f8ebe946a9db
Sabarish <absabarish20@gmail.com>
2026-04-02 01:36:59 +0530

    Simplified the TF

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-terraform-jenkins-statefile"
-    key            = "s3-provisioning/terraform.tfstate"
-    region         = "ap-south-1" 
+    bucket = "s3-terraform-jenkins-statefile"
+    key    = "s3-provisioning/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
 
@@ -10,5 +10,5 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket  = "jenkins-sample-s3bucket-as1-terraform"
+  bucket = "jenkins-sample-s3bucket-as1-terraform"
 }
