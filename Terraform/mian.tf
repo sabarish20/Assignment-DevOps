04d3073b5464cbec8f72110bca52d02d404cc867
Sabarish <85452305+sabarish20@users.noreply.github.com>
2026-04-02 00:26:29 +0530

    Create mian.tf

---
+++ Terraform/mian.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3-terraform-jenkins-statefile"
+    key            = "s3-provisioning/terraform.tfstate"
+    region         = "ap-south-1" 
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket  = "jenkins-sample-s3bucket-as1-terraform"
+}
