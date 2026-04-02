469590fbaaad41cc16721a44e3aa845ded7886c7
Sabarish <absabarish20@gmail.com>
2026-04-02 05:40:52 +0530

    ALB Controller

---
+++ Terraform/modules/EKS/alb-role.tf
@@ -0,0 +1,46 @@
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.k8s.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.k8s.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name   = "${var.eks_name}-AWSLoadBalancerControllerIAMPolicy"
+  policy = file("${path.module}/alb_iam_policy.json")
+}
+
+data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
+  name               = "${var.eks_name}-AmazonEKSLoadBalancerControllerRole"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
+
+output "aws_load_balancer_controller_role_arn" {
+  value = aws_iam_role.aws_load_balancer_controller.arn
+}
